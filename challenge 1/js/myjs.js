a2d7a6bd2f508acfc140d02b2b373c38ea47c6ca
T4L3NT <36267395+T4L3NT@users.noreply.github.com>
2018-03-23 15:58:50 -0400

    Added a variable and function

--- challenge 1/js/myjs.js
+++ challenge 1/js/myjs.js
@@ -4,5 +4,9 @@
 function myFunction() {
     document.getElementById("text").innerHTML = "gotem";
 }
+var cook = function(recipe) {
+	console.log("In this recipe you have to" + "" + recipe);
+};
+recipe("use 3 teaspoons of sugar")
 
 
