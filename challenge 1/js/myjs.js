743371c36a9f36d6e31afcf8d54b5a355c0902ae
T4L3NT <36267395+T4L3NT@users.noreply.github.com>
2018-03-23 21:01:58 -0400

    Friday

--- challenge 1/js/myjs.js
+++ challenge 1/js/myjs.js
@@ -1,13 +1,18 @@
  document.getElementById("myButton").onclick = function () {
         location.href = "test.html";
     };
-
-var cook = function(recipe) {
-	console.log("In this recipe you have to" + "" + recipe);
-};
-recipe("use 3 teaspoons of sugar")
 function thankYou() {
 	alert("Thank you for answering the survey!");
 }
-
-
+function goneImage(){
+	$("img").hide();
+}
+function seeImage(){
+	$("img").show();
+}
+$("#hide").click( () => {
+	goneImage();
+	});
+$("#show").click( () => {
+	seeImage();
+	});
